55e34a409ebd8b01b64671077f3d1469080db283
RogerKluna <tsukino-l@hotmail.com>
2017-09-22 11:30:10 -0600

    Primer commit de prueba

---
+++ app/src/main/java/com/proyect/gitprueba/EscuchaActivity.java
@@ -0,0 +1,13 @@
+package com.proyect.gitprueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EscuchaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escucha);
+    }
+}
